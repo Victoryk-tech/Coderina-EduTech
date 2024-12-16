7d2515fd80199aec78b10506ad584fcffd184174
VictoryK-tech <kemelevictory3802@gmail.com>
2024-12-16 16:04:10 +0100

    added

--- client/app/About/components/Team.jsx
+++ client/app/About/components/Team.jsx
@@ -16,23 +16,18 @@ const Team = () => {
       text: "Founder",
     },
     {
-      img: founder2,
-      name: "Mr Oluwabisi Kelvin Ajayi",
-      text: "Director of programs and engagements",
-    },
-    {
       img: founder3,
       name: "Mr Femi Niyi",
       text: "Chairman Board of trustee",
     },
     {
-      img: founder1,
-      name: "Mr Olajide Ajayi",
-      text: "Founder",
+      img: founder2,
+      name: "Mr Olabisi Kelvin Ajayi",
+      text: "Director of programs and engagements",
     },
   ];
   return (
-    <div className="px-4 md:px-4 lg:px-16 py-12 bg-white">
+    <div className="px-4 md:px-4 lg:px-20 py-12 bg-white w-full">
       {" "}
       <div className="team__container">
         <div className="flex items-center justify-between">
@@ -41,18 +36,45 @@ const Team = () => {
             Volunteer with us <FaArrowRightLong />
           </CustomButton>
         </div>
-        <div className="grid md:grid-cols-4 items-center justify-center gap-x-3">
+        {/* <div className="w-full grid md:grid-cols-3 items-center justify-between space-y-10 md:space-y-0 md:gap-x-3">
           {ourTeamCard.map((teamCard, i) => {
             return (
               <div
-                className="w-full md:w-[250px] h-[300px]  md:space-y-3 rounded-2xl"
+                className="w-full md:w-[300px] md:h-[300px]  md:space-y-3 rounded-2xl"
+                key={i}
+              >
+                <div className="w-full h-[240px] md:w-full md:h-full">
+                  <Image
+                    src={teamCard.img}
+                    alt="image"
+                    className="w-full h-full object-cover rounded-2xl"
+                  />
+                </div>
+                <div className="space-y-1">
+                  <h3 className="font-medium text-[17px] text-center">
+                    {teamCard.name}
+                  </h3>
+                  <h3 className="font-normal text-[13px] text-center">
+                    {teamCard.text}
+                  </h3>
+                </div>
+              </div>
+            );
+          })}
+        </div> */}
+
+        <div className="w-full grid md:grid-cols-3 items-start md:items-center justify-between space-y-[5rem] md:space-y-0 ">
+          {ourTeamCard.map((teamCard, i) => {
+            return (
+              <div
+                className="w-full md:w-[250px] h-[400px]  md:space-y-8"
                 key={i}
               >
                 <div className="w-full">
                   <Image
                     src={teamCard.img}
                     alt="image"
-                    className="w-full h-full object-contain rounded-2xl"
+                    className="w-full h-full object-cover rounded-2xl"
                   />
                 </div>
                 <div className="space-y-1">
